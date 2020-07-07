725c17078f4ddb07e9d14792252eb66df91c254b
luojiongtian <luojiongtian@sina.com>
2020-07-07 15:24:07 +0800

    update

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,11 +1,4 @@
-/*
- * fluro
- * Created by Yakka
- * https://theyakka.com
- * 
- * Copyright (c) 2019 Yakka, LLC. All rights reserved.
- * See LICENSE for distribution and usage details.
- */
+
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import './route_handlers.dart';
